ef501fc8dd85219578378a8512172db436707cf7
dlguszoo <lhjivy1024@gmail.com>
2026-05-20 14:19:02 +0900

    [#1] fix: appSchema static let 사용하여 lazy + thread-safe 초기화 캐싱

--- Projects/DVData/Sources/Storage/Local/Models/AppSchema.swift
+++ Projects/DVData/Sources/Storage/Local/Models/AppSchema.swift
@@ -7,16 +7,14 @@ enum SwiftDataModel { }
 extension Schema {
     private static let schemaVersion: Schema.Version = Version(1, 0, 0)
 
-    static var appSchema: Schema {
-        Schema([
-            SwiftDataModel.Project.self,
-            SwiftDataModel.Secret.self,
-            SwiftDataModel.SecretProjectLink.self,
-            SwiftDataModel.SecretPayload.self,
-            SwiftDataModel.SecretMetadata.self,
-            SwiftDataModel.SecretAuditLog.self,
-            SwiftDataModel.AppAuditLog.self,
-            SwiftDataModel.BackupRecord.self,
-        ], version: schemaVersion)
-    }
+    static let appSchema = Schema([
+        SwiftDataModel.Project.self,
+        SwiftDataModel.Secret.self,
+        SwiftDataModel.SecretProjectLink.self,
+        SwiftDataModel.SecretPayload.self,
+        SwiftDataModel.SecretMetadata.self,
+        SwiftDataModel.SecretAuditLog.self,
+        SwiftDataModel.AppAuditLog.self,
+        SwiftDataModel.BackupRecord.self,
+    ], version: schemaVersion)
 }
