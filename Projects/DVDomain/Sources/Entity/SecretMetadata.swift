8320c43d55e9339320414f80f0f86102b72016f7
dlguszoo <lhjivy1024@gmail.com>
2026-05-23 15:52:48 +0900

    [#12] feat: Secret, Metadata, Payload entity 구현

--- Projects/DVDomain/Sources/Entity/SecretMetadata.swift
+++ Projects/DVDomain/Sources/Entity/SecretMetadata.swift
@@ -2,14 +2,17 @@
 
 import Foundation
 
-public struct SecretMetadata: Equatable, Sendable {
+public struct SecretMetadata: Equatable, Identifiable, Sendable {
+    public var id: UUID
     public var metadataJSON: Data
     public var schemaVersion: Int
 
     public init(
+        id: UUID,
         metadataJSON: Data,
         schemaVersion: Int
     ) {
+        self.id = id
         self.metadataJSON = metadataJSON
         self.schemaVersion = schemaVersion
     }
