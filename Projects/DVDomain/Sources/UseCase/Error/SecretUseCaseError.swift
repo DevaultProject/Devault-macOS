79e1a8cae22defba1d20089f9bb82ac938a01a51
dlguszoo <lhjivy1024@gmail.com>
2026-05-23 17:47:26 +0900

    [#12] feat: SecretUseCaseError 정의 및 mapping 함수 구현

---
+++ Projects/DVDomain/Sources/UseCase/Error/SecretUseCaseError.swift
@@ -0,0 +1,33 @@
+// Copyright © 2026 Devault. All rights reserved
+
+import Foundation
+
+public enum SecretUseCaseError: Error, Equatable, Sendable {
+    case invalidName
+    case invalidSecretType
+    case secretNotFound(id: UUID)
+    case repositoryFailure(SecretRepositoryError)
+    case cryptoFailure(SecretCryptoError)
+    case unexpected
+}
+
+extension SecretUseCaseError {
+    static func map(_ error: Error) -> SecretUseCaseError {
+        if let error = error as? SecretUseCaseError {
+            return error
+        }
+
+        if let error = error as? SecretRepositoryError {
+            if case let .notFound(id) = error {
+                return .secretNotFound(id: id)
+            }
+            return .repositoryFailure(error)
+        }
+
+        if let error = error as? SecretCryptoError {
+            return .cryptoFailure(error)
+        }
+
+        return .unexpected
+    }
+}
