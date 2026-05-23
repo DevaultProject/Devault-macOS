6db18bb2f977c45cf0d4a527e463788249bdfb1a
dlguszoo <lhjivy1024@gmail.com>
2026-05-23 22:20:47 +0900

    [#12] chore: Domain 모듈 Service 재폴더링

--- Projects/DVDomain/Sources/Security/Error/SecretCryptoError.swift
+++
@@ -1,9 +0,0 @@
-// Copyright © 2026 Devault. All rights reserved
-
-public enum SecretCryptoError: Error, Equatable, Sendable {
-    case keyUnavailable
-    case encryptionFailed
-    case decryptionFailed
-    case encodingFailed
-    case decodingFailed
-}
