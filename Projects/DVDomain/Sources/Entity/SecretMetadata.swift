eba8e4fbe4848bb113d9a5446158009a0cf7bc05
dlguszoo <lhjivy1024@gmail.com>
2026-05-23 17:00:25 +0900

    [#12] fix: SecretMetadata, Payload Entity에서 id 제거

--- Projects/DVDomain/Sources/Entity/SecretMetadata.swift
+++ Projects/DVDomain/Sources/Entity/SecretMetadata.swift
@@ -2,17 +2,14 @@
 
 import Foundation
 
-public struct SecretMetadata: Equatable, Identifiable, Sendable {
-    public var id: UUID
+public struct SecretMetadata: Equatable, Sendable {
     public var metadataJSON: Data
     public var schemaVersion: Int
 
     public init(
-        id: UUID,
         metadataJSON: Data,
         schemaVersion: Int
     ) {
-        self.id = id
         self.metadataJSON = metadataJSON
         self.schemaVersion = schemaVersion
     }
