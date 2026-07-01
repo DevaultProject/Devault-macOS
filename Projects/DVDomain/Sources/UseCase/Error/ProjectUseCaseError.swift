c23059b3bf87eb8e5cbdda8227bb62a48dd0745e
dlguszoo <lhjivy1024@gmail.com>
2026-07-01 14:28:42 +0900

    [#25] feat: ProjectUseCase Error 정의, 프로젝트 이름 validation Helper 구현

---
+++ Projects/DVDomain/Sources/UseCase/Error/ProjectUseCaseError.swift
@@ -0,0 +1,27 @@
+// Copyright © 2026 Devault. All rights reserved
+
+import Foundation
+
+public enum ProjectUseCaseError: Error, Equatable, Sendable {
+    case invalidName
+    case projectNotFound(id: UUID)
+    case repositoryFailure(ProjectRepositoryError)
+    case unexpected
+}
+
+extension ProjectUseCaseError {
+    static func map(_ error: Error) -> ProjectUseCaseError {
+        if let error = error as? ProjectUseCaseError {
+            return error
+        }
+
+        if let error = error as? ProjectRepositoryError {
+            if case let .notFound(id) = error {
+                return .projectNotFound(id: id)
+            }
+            return .repositoryFailure(error)
+        }
+
+        return .unexpected
+    }
+}
