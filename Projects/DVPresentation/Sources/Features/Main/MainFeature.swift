599a742331aeaddb306a70bd75e3aea4a4efd182
dlguszoo <lhjivy1024@gmail.com>
2026-07-16 02:31:50 +0900

    [#37] feat: MainFeature 및 NavigationSplitView 기본 틀 구성

---
+++ Projects/DVPresentation/Sources/Features/Main/MainFeature.swift
@@ -0,0 +1,58 @@
+// Copyright © 2026 Devault. All rights reserved
+
+import SwiftUI
+
+import ComposableArchitecture
+
+// MARK: - MainFeature
+
+@Reducer
+public struct MainFeature {
+
+  // MARK: - State
+
+  @ObservableState
+  public struct State: Equatable {
+    public var columnVisibility: NavigationSplitViewVisibility = .all
+
+    public init() {}
+  }
+
+  // MARK: - Action
+
+  public enum Action: Equatable {
+
+    // MARK: - View
+
+    case task
+    case columnVisibilityChanged(NavigationSplitViewVisibility)
+
+    // MARK: - Delegate
+
+    case delegate(Delegate)
+
+    public enum Delegate: Equatable {}
+  }
+
+  // MARK: - Init
+
+  public init() {}
+
+  // MARK: - Body
+
+  public var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .task:
+        return .none
+
+      case .columnVisibilityChanged(let visibility):
+        state.columnVisibility = visibility
+        return .none
+
+      case .delegate:
+        return .none
+      }
+    }
+  }
+}
