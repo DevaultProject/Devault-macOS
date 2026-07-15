f39a854365d58ba0ba2be851065e3bc1624cb3ba
dlguszoo <lhjivy1024@gmail.com>
2026-07-16 02:31:50 +0900

    [#37] feat: MainFeature 및 NavigationSplitView 기본 틀 구성

---
+++ Projects/DVPresentation/Sources/Features/Main/MainView.swift
@@ -0,0 +1,67 @@
+// Copyright © 2026 Devault. All rights reserved
+
+import SwiftUI
+
+import ComposableArchitecture
+
+// MARK: - MainView
+
+struct MainView: View {
+
+  // MARK: - Properties
+
+  @Bindable var store: StoreOf<MainFeature>
+
+  // MARK: - Body
+
+  var body: some View {
+    content
+      .task { store.send(.task) }
+  }
+}
+
+// MARK: - Subviews
+
+extension MainView {
+
+  private var content: some View {
+    NavigationSplitView(
+      columnVisibility: Binding(
+        get: { store.columnVisibility },
+        set: { store.send(.columnVisibilityChanged($0)) }
+      )
+    ) {
+      sidebar
+    } content: {
+      contentColumn
+    } detail: {
+      detailColumn
+    }
+    .navigationSplitViewStyle(.balanced)
+    .toolbarBackground(.hidden, for: .windowToolbar)
+  }
+
+  private var sidebar: some View {
+    Text("Sidebar")
+  }
+
+  private var contentColumn: some View {
+    Text("Content")
+      .navigationTitle("")
+  }
+
+  private var detailColumn: some View {
+    Text("Detail")
+      .navigationTitle("")
+  }
+}
+
+// MARK: - Preview
+
+#Preview {
+  MainView(
+    store: Store(initialState: MainFeature.State()) {
+      MainFeature()
+    }
+  )
+}
