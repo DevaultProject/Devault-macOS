cd4c63d5d8117199a0e2ffa6d9de97780d1455c1
dlguszoo <lhjivy1024@gmail.com>
2026-07-24 00:10:23 +0900

    [#45] feat: DevaultApp 윈도우 전체화면 및 타이틀바 숨김 설정

--- Projects/Devault/Sources/DevaultApp.swift
+++ Projects/Devault/Sources/DevaultApp.swift
@@ -5,13 +5,17 @@ import DVPresentation
 
 @main
 struct DevaultApp: App {
-    var body: some Scene {
-        WindowGroup {
-            AppView(
-                store: Store(initialState: AppFeature.State()) {
-                    AppFeature()
-                }
-            )
+  var body: some Scene {
+    WindowGroup {
+      AppView(
+        store: Store(initialState: AppFeature.State()) {
+          AppFeature()
         }
+      )
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .ignoresSafeArea()
     }
+    .windowStyle(.hiddenTitleBar)
+    .defaultSize(width: 800, height: 600)
+  }
 }
