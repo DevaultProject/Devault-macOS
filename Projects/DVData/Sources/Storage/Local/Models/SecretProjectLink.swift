24fb2688a2103f5b017dfe1901e92199fbef332b
Hyeon-Ju <lhjivy1024@gmail.com>
2026-08-04 02:42:45 +0900

    [#64] fix: CloudKit 동기화 지연 시 Secret 조회/연결 안정화
    SecretProjectLink에 project/secret id를 스냅샷으로 보관해 관계 동기화 지연 중에도 목록 조회와 프로젝트 연결 갱신이 흔들리지 않게 한다.

--- Projects/DVData/Sources/Storage/Local/Models/SecretProjectLink.swift
+++ Projects/DVData/Sources/Storage/Local/Models/SecretProjectLink.swift
@@ -8,6 +8,12 @@ extension SwiftDataModel {
         var linkKey: String = ""
         var linkedAt: Date = Date()
 
+        // 관계(project/secret)가 아직 CloudKit에서 동기화되지 않아 nil인 순간에도
+        // "이 링크가 어떤 project/secret을 가리키는지"를 판단할 수 있도록 스칼라로도 들고 있는다.
+        // (SecretAuditLog가 secretSnapshotId를 스냅샷으로 들고 있는 것과 같은 패턴)
+        var projectID: UUID = UUID()
+        var secretID: UUID = UUID()
+
         @Relationship
         var project: Project?
 
@@ -21,6 +27,8 @@ extension SwiftDataModel {
         ) {
             self.linkKey = "\(project.id.uuidString):\(secret.id.uuidString)"
             self.linkedAt = linkedAt
+            self.projectID = project.id
+            self.secretID = secret.id
             self.project = project
             self.secret = secret
         }
