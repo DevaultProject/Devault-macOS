6e4194676d48fe3c1f9a33db04f5fd66fa1bd90e
Hyeon-Ju <lhjivy1024@gmail.com>
2026-08-10 22:29:38 +0900

    [#81] chore: Repository/UseCase Interface 주석 보강
    SettingsRepository/DetectSecretUseCase/ICloudSyncSettingsUseCase/
    OnboardingStatusUseCase 중 주석 없던 메서드에 기존 컨벤션대로 doc comment 추가.

--- Projects/DVDomain/Sources/UseCase/Interface/Settings/ICloudSyncSettingsUseCase.swift
+++ Projects/DVDomain/Sources/UseCase/Interface/Settings/ICloudSyncSettingsUseCase.swift
@@ -3,6 +3,9 @@
 import Foundation
 
 public protocol ICloudSyncSettingsUseCase: Sendable {
+  /// iCloud 동기화 사용 여부를 확인한다.
   func isEnabled() -> Bool
+  /// iCloud 동기화 사용 여부를 저장한다.
+  /// - Parameter enabled: 사용 여부
   func setEnabled(_ enabled: Bool)
 }
